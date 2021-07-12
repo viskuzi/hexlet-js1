a1816bdf3381e5757a4f1f7d867f1875ad7d3a2d
Константин Лущик <viskuzi1987@gmail.com>
2021-07-13 02:12:29 +0300

    install eslint

--- hexlet-hello-world/index.js
+++ hexlet-hello-world/index.js
@@ -1,6 +1,8 @@
-import _ from "lodash";
-console.log(_.last(["one", "two"]));
+import _ from 'lodash';
+
+console.log(_.last(['one', 'two']));
 const fn = (a, b) => {
-  c = a + b;
+  const c = a + b;
   return c;
 };
+fn(1, 2);
