a4c4a424173682c3432f52bf11515c32235a484c
Константин Лущик <viskuzi1987@gmail.com>
2021-10-29 20:33:22 +0300

    created folder tests and src

---
+++ run.js
@@ -0,0 +1,35 @@
+урок Функции.Сигналы
+
+import { get } from 'lodash';
+const emails = [
+  'info@gmail.com',
+  'info@yandex.ru',
+  'info@hotmail.com',
+  'mk@host.com',
+  'support@hexlet.io',
+  'key@yandex.ru',
+  'sergey@gmail.com',
+  'vovan@gmail.com',
+  'vovan@hotmail.com',
+];
+const freeEmailDomains = [
+  'gmail.com',
+  'yandex.ru',
+  'hotmail.com',
+  'yahoo.com',
+];
+
+const getFreeDomainsCount = (emails) => emails
+  .map((email) => {
+    const [, domain] = email.split('@');
+    return domain;
+  })
+  .filter((domain) => freeEmailDomains.includes(domain))
+  .reduce((acc, domain) => {
+    const count = get(acc, domain, 0) + 1;
+    return { ...acc, [domain]: count };
+  }, {});
+
+// END
+
+getFreeDomainsCount(emails); 
